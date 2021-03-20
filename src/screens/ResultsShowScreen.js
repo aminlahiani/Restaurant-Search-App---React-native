9fb59441c83e34fa03d286fc4d06765b62bd306a
amin lahiai <aminlahiani@outlook.com>
2021-03-20 16:08:53 +0100

    Showing a List of Images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet , Image , FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = () => {
   const route = useRoute();
   const id = route.params.id
   console.log(id)
+  const [result, setResult] = React.useState(null);
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  React.useEffect(() => {
+    getResult(id);
+  }, []);
+  if (!result) {
+    return null;
+  }
   
   return (
     <View>
-      <Text>Results Show</Text>
-    
+    <Text>{result.name}</Text>
+    <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15
+  },
+  image: {
+    height: 200,
+    width: 300,
+    borderRadius: 4,
+    marginBottom: 5
+  },
+  name: {
+    fontWeight: 'bold'
+  }
+});
 
 export default ResultsShowScreen;
